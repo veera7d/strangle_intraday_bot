7613a08f5959156ac1f493a028149d98aec1b17e
Hanuma <hanumad7@gmail.com>
2023-08-30 02:52:43 -0500

    added option chain

--- strangle.js
+++ strangle.js
@@ -5,30 +5,36 @@ const constants = require("./constants");
 const fs = require("fs");
 const requests = require("./requests");
 
-const DCN = 200;
+const DCN = 100;
 
 const script = constants.scripts.FINNIFTY;
 const expiry = "29AUG23";
 const lots = 3;
 
 let orders = [];
+let day_SL_rupees = -200;
+let booked_pnl = 0;
 
 let CE = {
     leg: "CE",
     ltp: 0,
-    strike: 19850,
+    strike: 19900,
     token_obj: null,
     locked: false,
-    order_id: null
+    order_id: null,
+    pnl: 0,
+    entry: 0
 }
 
 let PE = {
     leg: "PE",
     ltp: 0,
-    strike: 19450,
+    strike: 19900,
     token_obj: null,
     locked: false,
-    order_id: null
+    order_id: null,
+    pnl: 0,
+    entry: 0
 }
 
 const strike_update = async (CE_PE, strike_diff) => {
@@ -52,6 +58,21 @@ const risk_on_off = () => {
     }
 }
 
+const exit_all = async () => {
+    //await place(CE, constants.order_action.BUY);
+    orders.push({ leg: CE.leg, action: constants.order_action.BUY, ltp: CE.ltp, strike: CE.strike });
+
+    //await place(PE, constants.order_action.BUY);
+    orders.push({ leg: PE.leg, action: constants.order_action.BUY, ltp: PE.ltp, strike: PE.strike });
+}
+
+const calculate_PNL = () => {
+    CE.pnl = CE.entry - CE.ltp;
+    PE.pnl = PE.entry - PE.ltp;
+    process.stdout.write(" CE " + CE.ltp + " PE " + PE.ltp + " TPNL " + ((booked_pnl + CE.pnl + PE.pnl) * constants.lot_size[script]).toFixed(2).toString() + "         ");
+    process.stdout.write('\r');
+}
+
 const place = async (now, action) => {
     //orders.push({ leg: CE.leg, action: constants.order_action.SELL, ltp: CE_PE.ltp });
     now.locked = true;
@@ -72,20 +93,26 @@ const adjust = async (CE_PE, strike_diff) => {
     //square corrent leg;
     //await place(CE_PE, constants.order_action.BUY);
     orders.push({ leg: CE_PE.leg, action: constants.order_action.BUY, ltp: CE_PE.ltp, strike: CE_PE.strike });
-
+    booked_pnl = booked_pnl + CE_PE.pnl;
     //change the strike
     strike_update(CE_PE, strike_diff);
     await update_ltps();
+
     // open adjusted leg;
     //await place(CE_PE, constants.order_action.SELL);
     orders.push({ leg: CE_PE.leg, action: constants.order_action.SELL, ltp: CE_PE.ltp, strike: CE_PE.strike });
-
+    CE_PE.entry = CE_PE.ltp;
 
     fs.promises.writeFile("./savestate.json", JSON.stringify({ "CE": CE, "PE": PE, 'orders': orders }));
 }
 
 let rules = async () => {
     await update_ltps();
+    calculate_PNL();
+    if ((booked_pnl + CE.pnl + PE.pnl) * constants.lot_size[script] < day_SL_rupees) {
+        await exit_all()
+        return;
+    }
     if (CE.ltp >= 2 * PE.ltp) {
         (risk_on_off() === true) ? await adjust(PE, constants.steps[script]) : await adjust(CE, constants.steps[script]);
     } else if (PE.ltp >= 2 * CE.ltp) {
@@ -103,8 +130,10 @@ const init = async () => {
     await update_ltps();
     //await place(CE, constants.order_action.SELL);
     orders.push({ leg: CE.leg, action: constants.order_action.SELL, ltp: CE.ltp, strike: CE.strike });
+    CE.entry = CE.ltp;
     //await place(PE, constants.order_action.SELL);
     orders.push({ leg: PE.leg, action: constants.order_action.SELL, ltp: PE.ltp, strike: PE.strike });
+    PE.entry = PE.ltp;
     await rules();
     fs.promises.writeFile("./savestate.json", JSON.stringify({ "CE": CE, "PE": PE, 'orders': orders }));
 }
--- util.js
+++ util.js
@@ -1,4 +1,7 @@
 const fs = require("fs")
+const orders = require("./orders");
+const constants = require("./constants");
+
 const get_token_with_symbol_exchange = async (symbol, exchange) => {
     try {
         let data = await fs.promises.readFile("./response.json", 'utf8');
@@ -10,9 +13,87 @@ const get_token_with_symbol_exchange = async (symbol, exchange) => {
     }
 }
 
+const get_ATM_strike = (script, ltp) => {
+    step = constants.steps[script];
+    rem = ltp % step;
+    base = ltp - rem;
+    if (ltp > base + (step / 2)) {
+        return base + step;
+    }
+    return base;
+}
+
 let get_token_obj = async (script, exp, strike, cepe) => {
     return await get_token_with_symbol_exchange(script + exp + strike.toString() + cepe, "NFO")
 }
 
+let get_option_chain_ltps = async (script, exp, no_of_strikes, CE_PE) => {
+    try {
+        let script_ltp = await orders.get_ltp(await get_token_with_symbol_exchange(script, constants.exchanges.NSE));
+        let atm = get_ATM_strike(script, script_ltp);
+        if (CE_PE === constants.option_types.CE) {
+            atm = atm + (10 * constants.steps[script]);
+        } else if (CE_PE === constants.option_types.PE) {
+            atm = atm - (10 * constants.steps[script]);
+        }
+        let list = [];
+        let promises = [];
+        for (let i = 0; i < no_of_strikes; i++) {
+            let strike = atm;
+            let token_obj = await get_token_obj(script, exp, strike, CE_PE);
+            promises.push(orders.get_ltp(token_obj));
+            list.push({ strike, token_obj });
+            atm = (CE_PE === constants.option_types.CE) ? atm - constants.steps[script] : atm + constants.steps[script];
+        }
+        let values = await Promise.all([...promises]).then(async (values) => {
+            for (let i = 0; i < values.length; i++) {
+                if (values[i] == undefined) {
+                    values[i] = await orders.get_ltp(list[i].token_obj);
+                }
+            }
+            console.log(values);
+            return values;
+        }).catch(async (exception) => console.log("exception: ", exception))
+        return values;
+    }
+    catch (exception) {
+        console.log("get_option_chain_ltps exception: ", exception);
+    }
+}
 
-module.exports = { get_token_obj }
+let get_near_number = (value, list) => {
+    let diff = []
+    let min = 100;
+    let min_index = -1;
+    for (let i = 0; i < list.length; i++) {
+        let d = Math.abs(list[i] - value);
+        if (d < min) {
+            min = d;
+            min_index = i;
+        }
+        diff.push(d);
+    }
+    return list[min_index];
+}
+
+let get_strike_near_to_price = async (script, exp, price, CE_PE) => {
+    let CE_PE_values = { PE: null, CE: null };
+    let CE_PE_ltps = { PE: null, CE: null };
+    if (CE_PE == constants.option_types.CE) {
+        CE_PE_values.CE = await get_option_chain_ltps(script, exp, 15, constants.option_types.CE);
+    } else if (CE_PE == constants.option_types.PE) {
+        CE_PE_values.PE = await get_option_chain_ltps(script, exp, 15, constants.option_types.PE);
+    } else {
+        CE_PE_values.CE = await get_option_chain_ltps(script, exp, 15, constants.option_types.CE);
+        CE_PE_values.PE = await get_option_chain_ltps(script, exp, 15, constants.option_types.PE);
+    }
+    if (CE_PE_values.CE != null) {
+        CE_PE_ltps.CE = get_near_number(price, CE_PE_values.CE);
+    }
+    if (CE_PE_values.PE != null) {
+        CE_PE_ltps.PE = get_near_number(price, CE_PE_values.PE);
+    }
+    return CE_PE_ltps;
+}
+
+module.exports = { get_token_obj, get_strike_near_to_price }
